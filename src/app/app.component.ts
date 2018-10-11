a3ecc29172e29e9f56876b55ecdd067ed2302404
GaganSuneja <gagan@sumtracker.com>
2018-10-11 16:19:33 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import {HomePage} from '../pages/home/home';
+import {ListPage} from '../pages/list/list';
+import {firebaseConfig} from '../constants/constants';
+
+import * as firebase from 'firebase';
 
 @Component({
   templateUrl: 'app.html'
@@ -14,15 +17,18 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
+  searchTerm: string;
+  firebaseDbref: any;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      {title: 'Home', component: HomePage},
+      {title: 'List', component: ListPage}
     ];
 
   }
@@ -34,11 +40,21 @@ export class MyApp {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(JSON.parse(firebaseConfig), 'myApp');
   }
 
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  getUserData() {
+    this.firebaseDbref = firebase.database();
+    this.firebaseDbref.ref('/user_data/skrm/car_list/' + this.searchTerm.toLowerCase())
+      .once('value')
+      .then(function (snapshot) {
+        console.log(snapshot.val());
+      });
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {firebaseConfig} from '../../constants/constants';
+import * as firebase from "firebase";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  firebaseDbref: any;
+  searchTerm: string;
+  userData = {
+    'contact': '',
+    'name': '',
+    'car_no': ''
+  };
+  postData: any;
+  dbCarRefPath = '/user_data/skrm/car_list/';
 
   constructor(public navCtrl: NavController) {
-
+    this.firebaseDbref = firebase.apps[0].database();
+    this.intializePostData();
   }
 
+  intializePostData() {
+    this.postData = {
+      'contact': undefined,
+      'name': undefined,
+      'car_no': undefined
+    };
+  }
+
+  getUserData() {
+    const searchTerm = this.dbCarRefPath + this.convertToUpperCase(this.searchTerm);
+    this.firebaseDbref.ref(searchTerm)
+      .once('value')
+      .then((snapshot) => {
+        if (snapshot.val()) {
+          this.userData = snapshot.val();
+          this.userData.contact = '+91' + this.userData.contact;
+        }
+      });
+  }
+
+  addUser() {
+    this.postData.car_no = this.convertToUpperCase(this.postData.car_no);
+    this.firebaseDbref.ref(this.dbCarRefPath + this.postData.car_no).set(this.postData);
+    this.intializePostData();
+    // db.set(dataToPost);
+  }
+
+  convertToUpperCase(carNo) {
+    return carNo.toUpperCase();
+  }
+
+  // listUser(){
+  //   this.firebaseDbref.ref(this.dbCarRefPath)
+  //
+  // }
 }
